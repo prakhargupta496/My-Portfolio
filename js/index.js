388aec3d929614996f9bd44e5bf2e7aa6e32d171
Prakhar Gupta <54628037+prakhargupta496@users.noreply.github.com>
2021-01-26 23:01:44 +0530

    My portfolio site

---
+++ js/index.js
@@ -0,0 +1,14 @@
+// $(".email").hover(function(){
+
+//     var size = $(this).css("font-size");
+
+//     $(this).stop().animate({
+//         fontSize: "25px"
+//     }, 200);    
+
+// },function(){
+
+//     $(this).stop().animate({
+//         fontSize: "25px"
+//     }, 200);
+// });
